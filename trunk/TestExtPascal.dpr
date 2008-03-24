5bcf122c012165c31f2827d94d90b261a7bb38f1
wanderlan.anjos <wanderlan.anjos@0bf252ff-4e48-0410-9925-dbe306d98b98>
2008-03-24 23:24:38 +0000

    version 0.2

--- trunk/TestExtPascal.dpr
+++ trunk/TestExtPascal.dpr
@@ -5,6 +5,19 @@ program TestExtPascal;
 uses
   ExtPascal, Ext, ExtGlobal, ExtUtil, ExtAir, ExtData, ExtDD, ExtForm, ExtLayout, ExtMenu, ExtState, ExtTree, ExtGrid;
 
+var
+  W : ExtWindow;
 begin
+  W := ExtWindow.Create;
+  with W do begin
+    layout := 'fit';
+    width := 300;
+    height:= 150;
+    closable := false;
+    resizable := false;
+    plain := true;
+    items := 'dsdsd';
+    show;
+  end;
 end.
 
