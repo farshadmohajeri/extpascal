041c3c849724844ca37807bb0eec5f8c23f06a66
wanderlan.anjos <wanderlan.anjos@0bf252ff-4e48-0410-9925-dbe306d98b98>
2008-03-24 18:08:37 +0000

    Generated units compile ok.

--- ExtPascal.pas
+++ ExtPascal.pas
@@ -5,13 +5,37 @@ interface
 type
   ExtObject = class end;
   ArrayOfVariant = array of Variant;
-  HTMLElement = class end;
-  Element = string;
-  StyleSheet = string;
-  RegExp = class end;
-  CSSRule = string;
-  XMLDocument = string;
-  NodeList = string;
+  MixedCollection = ArrayOfVariant;
+  HTMLElement = class(ExtObject) end;
+  Element = class(ExtObject) end;
+  StyleSheet = class(ExtObject) end;
+  RegExp = class(ExtObject) end;
+  CSSRule = class(ExtObject) end;
+  XMLDocument = class(ExtObject) end;
+  NodeList = class(ExtObject) end;
+  Region = class(ExtObject) end;
+  NativeMenu = ExtObject;
+  el = type string;
+  Event = class(ExtObject) end;
+  HTMLNode = ExtObject;
+  _Constructor = class(ExtObject) end;
+  _Class = class(ExtObject) end;
+  ExtLibRegion = Region; //doc fault
+  visMode = Integer; // doc fault
+  The = ExtObject; // doc fault
+  This = ExtObject; // doc fault
+  airNativeMenu = ExtObject;
+  X = ExtObject; // doc fault
+  N1 = ExtObject; // doc fault
+  N2 = ExtObject; // doc fault
+  Layout = ExtObject; // Poor hierarchy definition
+  Id = ExtObject;// doc fault
+  iPageX = ExtObject; // doc fault
+  iPageY = ExtObject; // doc fault
+  ExtGridGrid = ExtObject; // doc fault
+  TreeSelectionModel = ExtObject; // doc fault
+  SelectionModel = ExtObject; // doc fault
+  DataSource = ExtObject; // doc fault
 
 implementation
 
