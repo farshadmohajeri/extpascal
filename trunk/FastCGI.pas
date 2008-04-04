d27c42e1bde950601069cfb71827f5d7d74d9cbd
wanderlan.anjos <wanderlan.anjos@0bf252ff-4e48-0410-9925-dbe306d98b98>
2008-04-04 18:39:33 +0000

    Continuing FastCGI implementation

--- trunk/FastCGI.pas
+++ trunk/FastCGI.pas
@@ -12,22 +12,45 @@ type
   TRecType = (rtBeginRequest = 1, rtAbortRequest, rtEndRequest, rtParams, rtStdIn, rtStdOut, rtStrErr, rtData, rtGetValues, rtGetValuesResult, rtUnknown);
   TRole = (rResponder = 1, rAuthorizer, rFilter);
   TProtocolStatus = (psRequestComplete, psCantMPXConn, psOverloaded, psUnknownRole);
-  TFastCGI = class
-    RequestID : integer;
-    Role : TRole;
-    Socket : TBlockSocket;
-    Variables : TStringList;
-    constructor Create;
-    destructor Destroy; override;
-    procedure SendRecord(S : string; pRecType : TRecType = rtStdOut);
-    function RecvRecord: string;
+
+  TFastCGIThread = class(TThread)
+  protected
+    FRequestID : integer;
+    FRole : TRole;
+    FSocket : TBlockSocket;
+    FKeepConn : boolean;
+    procedure AddParam(var S: string; Param: array of string);
     procedure ReadParams(Variables : TStringList; Params: string);
     procedure ReadBeginRequest(Buffer: string);
     procedure GetValues(Params: string);
+  public
+    Variables : TStringList;
+    property RequestID : integer read FRequestID;
+    property Role : TRole read FRole;
+    constructor Create(NewSocket : integer);
+    destructor Destroy; override;
+    procedure SendRecord(S : string; pRecType : TRecType = rtStdOut);
+    procedure Execute; override;
     procedure SendEndRequest(Status: TProtocolStatus = psRequestComplete);
   end;
 
+  TFastCGIApplication = class
+  private
+    WebServers : TStringList;
+  public
+    constructor Create;
+    destructor Destroy; override;
+    procedure Run;
+    function CanConnection(Address : string) : boolean;
+  end;
+
+var
+  FastCGIApplication : TFastCGIApplication;
+
 implementation
+
+uses
+  SysUtils;
 
 type
   THeader = packed record
@@ -46,56 +69,20 @@ type
     Filler2 : array[1..5] of byte;
   end;
 
-  TEndRequest = packed record
-    Header : THeader;
-    ApplicationStatus : Int64;
-    Response : TProtocolStatus;
-    Filler : array[1..3] of byte;
-  end;
-
-  TUnknownRequest = packed record
-    Header : THeader;
-    UnknownType : TRecType; // rebate unknown TRecType value > GetValuesResult
-    Filler : array[1..7] of byte;
-  end;
-
-constructor TFastCGI.Create; begin
+constructor TFastCGIThread.Create(NewSocket : integer); begin
+  FSocket := TBlockSocket.Create(NewSocket);
   Variables := TStringList.Create;
+  FreeOnTerminate := true;
+  inherited Create(false);
 end;
 
-destructor TFastCGI.Destroy; begin
+destructor TFastCGIThread.Destroy; begin
   Variables.Free;
+  FSocket.Free;
   inherited;
 end;
 
-procedure AddParam(var S : string; Param : array of string);
-var
-  Len : array[0..1] of Int64;
-  Format : array[0..1] of integer;
-  I, J : integer;
-begin
-  for I := 0 to 1 do begin
-    Len[I] := length(Param[I]);
-    if Len[I] <= 127 then
-      Format[I] := 1
-    else
-      Format[I] := 4;
-  end;
-  J := length(S);
-  SetLength(S, J + Len[0] + Format[0] + Len[1] + Format[1]);
-  inc(J);
-  for I := 0 to 1 do begin
-    if Format[I] = 1 then
-      S[J] := char(Len[I])
-    else
-      move(Len[I], S[J], Format[I]);
-    inc(J, Format[I]);
-  end;
-  move(Param[0][1], S[J], Len[0]);
-  move(Param[1][1], S[J + Len[0]], Len[1]);
-end;
-
-procedure TFastCGI.SendRecord(S : string; pRecType : TRecType = rtStdOut);
+procedure TFastCGIThread.SendRecord(S : string; pRecType : TRecType = rtStdOut);
 var
   Header : THeader;
   Buffer : string;
@@ -111,14 +98,33 @@ begin
   end;
   move(Header, Buffer[1], sizeof(Header));
   move(S[1], Buffer[sizeof(Header) + 1], length(S));
-  Socket.SendString(Buffer);
+  FSocket.SendString(Buffer);
 end;
 
-procedure TFastCGI.ReadParams(Variables : TStringList; Params : string);
+procedure TFastCGIThread.SendEndRequest(Status : TProtocolStatus = psRequestComplete); begin
+  SendRecord(#0#0#0 + char(Status) + char(Status), rtEndRequest);
+  Terminate;
+end;
+
+procedure TFastCGIThread.ReadBeginRequest(Buffer : string);
+var
+  BeginRequest : TBeginRequest;
+begin
+  move(Buffer[1], BeginRequest, sizeof(BeginRequest));
+  if BeginRequest.Role in [rResponder..rFilter] then begin
+    FRequestID := BeginRequest.Header.ID;
+    FRole      := BeginRequest.Role;
+    FKeepConn  := BeginRequest.KeepConn; // can't close socket if true
+  end
+  else
+    SendEndRequest(psUnknownRole);
+end;
+
+procedure TFastCGIThread.ReadParams(Variables : TStringList; Params : string);
 var
   I, Pos : integer;
-  Len : array[0..1] of Int64;
-  Param : array[0..1] of string;
+  Len    : array[0..1] of Int64;
+  Param  : array[0..1] of string;
 begin
   Pos := 1;
   while Pos < length(Params) do begin
@@ -141,28 +147,37 @@ begin
   end;
 end;
 
-procedure TFastCGI.ReadBeginRequest(Buffer : string);
+procedure TFastCGIThread.AddParam(var S : string; Param : array of string);
 var
-  BeginRequest : TBeginRequest;
+  I, J   : integer;
+  Len    : array[0..1] of Int64;
+  Format : array[0..1] of integer;
 begin
-  move(Buffer[1], BeginRequest, sizeof(BeginRequest));
-  if BeginRequest.Role in [rResponder..rFilter] then begin
-    RequestID := BeginRequest.Header.ID;
-    Role := BeginRequest.Role;
-  end
-  else
-    SendEndRequest(psUnknownRole);
+  for I := 0 to 1 do begin
+    Len[I] := length(Param[I]);
+    if Len[I] <= 127 then
+      Format[I] := 1
+    else
+      Format[I] := 4;
+  end;
+  J := length(S);
+  SetLength(S, J + Len[0] + Format[0] + Len[1] + Format[1]);
+  inc(J);
+  for I := 0 to 1 do begin
+    if Format[I] = 1 then
+      S[J] := char(Len[I])
+    else
+      move(Len[I], S[J], Format[I]);
+    inc(J, Format[I]);
+  end;
+  move(Param[0][1], S[J], Len[0]);
+  move(Param[1][1], S[J + Len[0]], Len[1]);
 end;
 
-procedure TFastCGI.SendEndRequest(Status : TProtocolStatus = psRequestComplete); begin
-  // Terminate := true;
-  SendRecord(#0#0#0 + char(Status) + char(Status), rtEndRequest);
-end;
-
-procedure TFastCGI.GetValues(Params : string);
+procedure TFastCGIThread.GetValues(Params : string);
 var
-  Variables : TStringList;
   I : integer;
+  Variables : TStringList;
   GetValuesResult : string;
 begin
   if Params = '' then exit;
@@ -178,26 +193,86 @@ begin
   SendRecord(GetValuesResult, rtGetValuesResult);
 end;
 
-function TFastCGI.RecvRecord : string;
+procedure TFastCGIThread.Execute;
 var
   Header : THeader;
   Buffer : string;
 begin
-  Result := '';
-  Buffer := Socket.RecvString;
-  move(Buffer[1], Header, sizeof(Header));
-  if (RequestID <> 0) and (Header.ID <> RequestID) then
-    SendEndRequest(psCantMPXConn)
-  else
-    case Header.RecType of
-      rtBeginRequest : ReadBeginRequest(Buffer);
-      rtAbortRequest : SendEndRequest;
-      rtParams : ReadParams(Variables, copy(Buffer, sizeof(Header) + 1, Header.Len));
-      rtStdIn, rtData : Result := copy(Buffer, sizeof(Header) + 1, Header.Len);
-      rtGetValues : GetValues(copy(Buffer, sizeof(Header) + 1, Header.Len));
+  try
+    if FastCGIApplication.CanConnection(FSocket.GetHostAddress) then
+      while not Terminated and FSocket.CanRead(200) do begin
+        Buffer := FSocket.RecvString;
+        if FSocket.SocketError = 0 then begin
+          move(Buffer[1], Header, sizeof(Header));
+          if (RequestID <> 0) and (Header.ID <> 0) and (Header.ID <> RequestID) then
+            SendEndRequest(psCantMPXConn)
+          else
+            case Header.RecType of
+              rtBeginRequest : ReadBeginRequest(Buffer);
+              rtAbortRequest : SendEndRequest;
+              rtParams       : ReadParams(Variables, copy(Buffer, sizeof(Header) + 1, Header.Len));
+              rtStdIn,
+              rtData         : ;//Result := copy(Buffer, sizeof(Header) + 1, Header.Len);
+              rtGetValues    : GetValues(copy(Buffer, sizeof(Header) + 1, Header.Len));
+            else
+              SendRecord(char(Header.RecType), rtUnknown);
+            end;
+        end
+        else
+          FSocket.Purge;
+      end
     else
-      SendRecord(char(Header.RecType), rtUnknown);
+      Terminate;
+  except
+    on E : Exception do begin
+      SendRecord(E.Message);
+      SendRecord(E.Message, rtStrErr);
     end;
+  end;
+end;
+
+{ TFastCGIApplication }
+
+function TFastCGIApplication.CanConnection(Address: string): boolean; begin
+  Result := (WebServers = nil) or (WebServers.IndexOf(Address) <> -1)
+end;
+
+constructor TFastCGIApplication.Create;
+var
+  WS : string;
+begin
+  WS := GetEnvironmentVariable('FCGI_WEB_SERVER_ADDRS');
+  if WS <> '' then begin
+    WebServers := TStringList.Create;
+    WebServers.DelimitedText := WS;
+  end;
+end;
+
+destructor TFastCGIApplication.Destroy; begin
+  WebServers.Free;
+  inherited;
+end;
+
+procedure TFastCGIApplication.Run;
+var
+  NewThread : integer;
+begin
+  try
+    with TBlockSocket.Create do
+      try
+        Bind(0, 10);
+        repeat
+          NewThread := Accept(200);
+          if NewThread <> 0 then TFastCGIThread.Create(NewThread);
+        until Terminated;
+      finally
+        FreeThreads;
+        Free;
+      end;
+  finally
+    fThreadListLock.Free;
+    FreeAndNil(fThreadList);
+  end;
 end;
 
 end.
