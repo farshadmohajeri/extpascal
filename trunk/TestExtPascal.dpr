9ef7b1ecc2dc36c2e98a994c6e36ae0a72c80520
wanderlan.anjos <wanderlan.anjos@0bf252ff-4e48-0410-9925-dbe306d98b98>
2008-05-10 15:49:46 +0000

    SetLength and ArryOfExtObject eliminated, using AddTo/Add and ExtobjectList instead

--- trunk/TestExtPascal.dpr
+++ trunk/TestExtPascal.dpr
@@ -3,147 +3,47 @@ program TestExtPascal;
 {$APPTYPE CONSOLE}
 
 uses
-  FCGIApp, SysUtils, ExtPascal, Ext, ExtGlobal, ExtData, ExtForm, ExtGrid;
+  FCGIApp, ExtPascal, Ext, ExtGlobal, ExtData, ExtForm, ExtGrid;
   //ExtUtil, ExtAir, ExtDD, ExtLayout, ExtMenu, ExtState, ExtTree;
 
 type
   TTestExtPascal = class(TExtThread)
-  public
-    I : integer;
-    Connection : ExtDataConnection;
-    DataRecord : ExtDataRecord;
-    Fields : ArrayOfExtDataField;
-    Login : ExtFormFormPanel;
-    TabActions, ActionPanel : ExtPanel;
-    TabPanel : ExtTabPanel;
-    Button : ExtButton;
-  published
+    Janela : ExtWindow;
     procedure Home; override;
-    procedure Animal;
   end;
 
-procedure TTestExtPascal.Animal; begin
-  Response := IntToStr(int64(Self)) + ': ' + Query['color'] + '; ' + Query['type'] + '; ' + IntToStr(I);
-  inc(I);
-end;
+{ TTestExtPascal }
 
 procedure TTestExtPascal.Home; begin
-  inherited;
-(*  Button := ExtButton.Create;
-  with Button do begin
-    RenderTo := 'content';
-    Handler  := _Function.JSFunction('', 'alert("You clicked ' + JSName + '");');
-    Text     := JSName;
-  end;
-  Login := ExtFormFormPanel.Create;
-  with Login do begin
-    LabelWidth   := 80;
-    Frame        := true;
-    HideBorders := true;
-    Title        := 'Login';
-    Width        := 300;
-    DefaultType  := 'textfield';
-    MonitorValid := true;
-    SetLengthButtons(1);
-    Buttons[0].Text := 'Login';
-    SetLengthItems(2, ExtFormTextField);
-    with ExtFormTextField(Items[0]) do begin FieldLabel := 'UserName'; Name := 'loginUserName'; AllowBlank := false end;
-    with ExtFormTextField(Items[1]) do begin FieldLabel := 'Password'; Name := 'loginPassword'; AllowBlank := false; InputType := 'password' end;
-  end;
-  with ExtWindow.Create do begin
-    Title    := JSName;
-    Layout   := 'form';
-    Height   := 160;
-    Width    := 300;
-    Closable := true;
-    Collapsible := true;
-    Resizable:= true;
-    Plain    := true;
-    SetLengthItems(1, NoCreate);
-    Items[0] := Login;
-    Show;
-  end;*)
-  (*
-  Connection := ExtDataConnection.Create;
-  with Connection do begin
-    Url := '/trabalho/extpascal/data.txt';
-    Method := 'GET';
-  end;
-  SetLength(Fields, 3, ExtDataField);
-  Fields[0].Name := 'status';
-  Fields[1].Name := 'report';
-  with Fields[2] do begin Name := 'duration'; _Type := 'int'; end;
-  DataRecord := ExtDataRecord.Create(Fields);
-  with ExtGridGridPanel.Create do begin
-    RenderTo := 'content';
-    Title := 'my Grid';
-    Width := 300;
-    Height := 200;
-    Frame := true;
-    Store := ExtDataStore.Create;
-    with Store do begin
-      AutoLoad := true;
-      Proxy := ExtDataHttpProxy.Create(Connection);
-      exit;
-      Reader := ExtDataJsonReader.Create(nil, DataRecord);
-      with ExtDataJsonReader(Reader) do begin
-        Root := 'rows';
-        TotalProperty := 'totalCount';
+  Janela := ExtWindow.Create;
+  with Janela do begin
+    Title := 'Hello Dialog';
+    Layout := 'fit';
+    Width := 500;
+    Height := 300;
+    CloseAction := 'hide';
+    Plain := true;
+    with ExtTabPanel.AddTo(Items) do begin
+      ActiveTab := 'tab1';
+      with ExtPanel.AddTo(Items) do begin
+        Id := 'tab1';
+        Title := 'Hello World 1';
+        Html := 'Hello...';
+      end;
+      with ExtPanel.AddTo(Items) do begin
+        Title := 'Hello World 2';
+        Html := '...World';
       end;
     end;
-    SetLengthColumns(3);
-    with Columns[0] do begin Header := '';         Width := 30;  Sortable := true; DataIndex := 'status';   Id := DataIndex; end;
-    with Columns[1] do begin Header := 'Name';     Width := 160; Sortable := true; DataIndex := 'report';   Id := DataIndex; end;
-    with Columns[2] do begin Header := 'Duration'; Width := 70;  Sortable := true; DataIndex := 'duration'; Id := DataIndex; end;
-  end;
-*)
-  TabActions := ExtPanel.Create;
-  with TabActions do begin
-    Frame := true;
-    Title := 'Actions';
-    Collapsible:= true;
-    ContentEl := 'actions';
-    TitleCollapse := true;
-  end;
-  ActionPanel := ExtPanel.Create;
-  with ActionPanel do begin
-    Id := 'action-panel';
-    Collapsible := true;
-    Width := 340;
-    Border := false;
-    BaseCls := 'x-plain';
-    SetLengthItems(1, NoCreate);
-    Items[0] := TabActions;
-    Region := 'west';
-    Split := true;
-    CollapseMode := 'mini';
-    MinWidth := 150;
-  end;
-(*  TabPanel := ExtTabPanel.Create;
-  with TabPanel do begin
-    DeferredRender := false;
-    AutoScroll := true;
-    ActiveTab := 'tab1'; // variant
-    Region := 'center';
-    Title := 'Main';
-    Closable := false;
-    Margins := '0 4 4 0';
-    SetLengthItems(2);
-    with Items[0] do begin Id := 'tab1'; ContentEl := 'tabs'; Title := 'Button';     Closable := false; AutoScroll := true end;
-    with Items[1] do begin Id := 'tab2'; ContentEl := 'tabs'; Title := 'Grid Panel'; Closable := false; AutoScroll := true end;
-  end;*)
-  with ExtViewPort.Create do begin
-    Layout := 'border';
-    SetLengthItems(1, NoCreate);
-    Items[0] := ActionPanel;
-//    Items[1] := TabPanel;
-  end;
-  with TabPanel do begin
-    Title := 'New Tab';
-    IconCls := 'tabs';
-    AutoLoad := ExtFormAction.Create;
-    ExtFormAction(AutoLoad).Url := 'TestExtPascal.exe';
-    Closable := true;
+    with ExtButton.AddTo(Buttons) do begin
+      Text := 'Submit';
+      Disabled := true;
+    end;
+    with ExtButton.AddTo(Buttons) do begin
+      Text := 'Close';
+      Handler := _Function.JSFunction('', Janela.JSName + '.hide()');
+    end;
+    Show;
   end;
 end;
 
