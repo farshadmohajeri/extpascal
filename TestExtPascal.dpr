546f1a671fd1b3ca76ef9abb4e54f6910fc443f9
wanderlan.anjos <wanderlan.anjos@0bf252ff-4e48-0410-9925-dbe306d98b98>
2008-05-20 21:33:47 +0000

    Ajax works! More examples.

--- TestExtPascal.dpr
+++ TestExtPascal.dpr
@@ -18,17 +18,19 @@ type
     procedure Layout;
     procedure AdvancedTabs;
     procedure AddTab;
+    procedure BorderLayout;
   end;
 
 procedure TSamples.Home;
 const
-  Examples : array[0..3] of record
+  Examples : array[0..4] of record
     Name, Proc, Gif, Desc : string
   end = (
     (Name: 'Basic TabPanel'; Proc: 'BasicTabPanel'; Gif: 'window'; Desc: 'Simple Hello World window that contains a basic TabPanel.'),
     (Name: 'Message Boxes';  Proc: 'MessageBoxes';  Gif: 'msg-box'; Desc: 'Different styles include confirm, alert, prompt, progress, wait and also support custom icons.'),
     (Name: 'Layout Window';  Proc: 'Layout';        Gif: 'window-layout'; Desc: 'A window containing a basic BorderLayout with nested TabPanel.'),
-    (Name: 'Advanced Tabs';  Proc: 'AdvancedTabs';  Gif: 'tabs-adv'; Desc: 'Advanced tab features including tab scrolling, adding tabs programmatically and a context menu plugin.')
+    (Name: 'Advanced Tabs';  Proc: 'AdvancedTabs';  Gif: 'tabs-adv'; Desc: 'Advanced tab features including tab scrolling, adding tabs programmatically and a context menu plugin.'),
+    (Name: 'Border Layout';  Proc: 'BorderLayout';  Gif: 'border-layout'; Desc: 'A complex BorderLayout implementation that shows nesting multiple components and sub-layouts.')
   );
 var
   I : integer;
@@ -37,7 +39,7 @@ begin
   SetStyle('img:hover{border:1px solid blue}');
   with ExtPanel.Create do begin
     Title       := 'ExtPascal Samples';
-    RenderTo    := 'content';
+    RenderTo    := 'body';
     Width       := 400;
     floating    := true;
     Collapsible := true;
@@ -109,7 +111,9 @@ procedure TSamples.AddTab; begin
     IconCls := 'tabs';
     Html := 'Tab Body ' + IntToStr(TabIndex) + '<br/><br/>blahblah';
     Closable := true;
+    Show;
   end;
+//  Tabs.ActiveTabNumber := TabIndex-1;
 end;
 
 procedure TSamples.AdvancedTabs;
@@ -117,16 +121,16 @@ var
   I : integer;
 begin
   SetStyle('.new-tab{background-image:url(/ext-2.1/examples/feed-viewer/images/new_tab.gif) !important}');
-  SetStyle('.tabs{background-image:url(/ext-2.1/examples/desktop/images/tabs.gif ) !important}');
+  SetStyle('.tabs{background-image:url(/ext-2.1/examples/desktop/images/tabs.gif) !important}');
   with ExtButton.Create do begin
-    RenderTo := 'content';
+    RenderTo := 'body';
     Text := 'Add Tab';
     IconCls := 'new-tab';
     Handler := _Function(Ajax('AddTab'));
   end;
   Tabs := ExtTabPanel.Create;
   with Tabs do begin
-    RenderTo := 'content';
+    RenderTo := 'body';
     ActiveTabNumber := 0;
     ResizeTabs := true; // turn on tab resizing
     MinTabWidth:= 115;
@@ -176,6 +180,103 @@ begin
   end;
 end;
 
+procedure TSamples.BorderLayout; begin
+(*  SetStyle('html,body{font:normal 12px verdana;margin:0;padding:0;border:0 none;overflow:hidden;height:100%} ' +
+	  'p{margin:5px}' +
+    '.settings{background-image:url(/ext-2.1/examples/shared/icons/fam/folder_wrench.png)}' +*)
+    SetStyle('.nav{background-image:url(/ext-2.1/examples/shared/icons/fam/folder_go.png)}');
+  with ExtViewport.Create do begin
+    Layout := 'border';
+    with ExtPanel.AddTo(Items) do begin
+      Region := 'north';
+      Height := 32;
+      Frame  := true;
+      Html   := '<p>north - generally for menus, toolbars and/or advertisements</p>';
+    end;
+    with ExtPanel.AddTo(Items) do begin
+      Region := 'south';
+      Html   := '<p>south - generally for informational stuff, also could be for status bar</p>';
+      Split  := true;
+      Height := 100;
+      //MinSize := 100;
+      //MaxSize := 200;
+      Collapsible := true;
+      Title := 'South';
+      Margins := '0 0 0 0';
+    end;
+    with ExtPanel.AddTo(Items) do begin
+      Region := 'east';
+//      ContentEl := 'south';
+      Split := true;
+      Height := 100;
+      //MinSize := 175;
+      //MaxSize := 400;
+      Collapsible := true;
+      Title := 'East Side';
+      Margins := '0 5 0 0';
+      Width := 225;
+      Layout := 'fit';
+      with ExtTabPanel.AddTo(Items) do begin
+        Border := false;
+        ActiveTabNumber := 1;
+        TabPosition := 'bottom';
+        with ExtPanel.AddTo(Items) do begin
+          Html := '<p>A TabPanel component can be a region.</p>';
+          Title := 'A Tab';
+          AutoScroll := true;
+        end;
+        with ExtGridPropertyGrid.AddTo(Items) do begin
+          Title := 'Property Grid';
+          Closable := true;
+          AddJS('source:{"(name)": "Property Grid", grouping: false, autoFitColumns: true, productionQuality: false,' +
+            'created: new Date(Date.parse("10/15/2006")), tested: false, version: .01, borderWidth: 1}');
+        end;
+      end;
+    end;
+    with ExtPanel.AddTo(Items) do begin
+      Region := 'west';
+      Id := 'west-panel';
+      Split := true;
+      Width := 200;
+      //MinSize := 175;
+      //MaxSize := 400;
+      Collapsible := true;
+      Title := 'West';
+      Margins := '0 0 0 5';
+      Layout := 'accordion';
+      //layoutConfig:{animate:true},
+      with ExtPanel.AddTo(Items) do begin
+//        ContentEl := 'west';
+        Title := 'Navigation';
+        Border := false;
+        IconCls := 'nav';
+      end;
+      with ExtPanel.AddTo(Items) do begin
+        Title := 'Settings';
+        Html := '<p>Some settings in here.</p>';
+        Border := false;
+        IconCls := 'settings';
+      end;
+    end;
+    with ExtTabPanel.AddTo(Items) do begin
+      Region := 'center';
+      DeferredRender := false;
+      ActiveTabNumber := 0;
+      with ExtPanel.AddTo(Items) do begin
+//        ContentEl := 'center1';
+        Title := 'Close Me';
+        Closable := true;
+        AutoScroll := true
+      end;
+      with ExtPanel.AddTo(Items) do begin
+//        ContentEl := 'center2';
+        Title := 'Center Panel';
+        AutoScroll := true
+      end;
+    end;
+  end;
+end;
+
 procedure TSamples.MessageBoxes;
 var
   ShowConfig : ExtShowConfig;
@@ -183,7 +284,7 @@ begin
   with ExtPanel.Create do begin
     Title    := 'Message Boxes';
     Width    := 700;
-    RenderTo := 'content';
+    RenderTo := 'body';
     Frame    := true;
     with ExtButton.AddTo(Buttons) do begin
       Text    := 'Alert Dialog';
