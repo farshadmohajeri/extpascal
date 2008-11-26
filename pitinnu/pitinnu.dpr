fa89a5179f040d79cd29064b88491a1db75aa76b
wanderlan.anjos <wanderlan.anjos@0bf252ff-4e48-0410-9925-dbe306d98b98>
2008-11-26 17:26:27 +0000

    Partial pitinnu upload

---
+++ pitinnu/pitinnu.dpr
@@ -0,0 +1,54 @@
+program pitinnu;
+
+{$IFDEF MSWINDOWS}{$APPTYPE CONSOLE}{$ENDIF}
+
+uses
+  FCGIApp, Classes, SysUtils, {$IFDEF MSWINDOWS}Services,{$ENDIF}
+  pitPrevalence, pitUtils, pitThread, pitModel, pitinnuModel, pitServer;//, pitSupportGDB, pitProxyDebugger;
+
+{$IFDEF MSWINDOWS}
+type
+  TServiceThread = class(TThread)
+    procedure Execute; override;
+  end;
+
+procedure TServiceThread.Execute; begin
+  Application.Run(Self)
+end;
+{$ENDIF}
+
+begin
+  DateSeparator := '/'; ShortDateFormat := 'd/M/yyyy';
+  TimeSeparator := ':'; ShortTimeFormat := 'hh:mm';
+  FileMode      := fmShareDenyWrite + fmOpenReadWrite;
+{$IFDEF MSWINDOWS} 
+  Service       := TService.Create(ServerName, PrevVersion + ' - ' + GetEnvironment);
+  with Service do try
+    if Install then
+      writeln('Service installed')
+    else if Uninstall then
+      writeln('Service uninstalled')
+    else begin
+      Prevalence  := TPrevalence.Create(ServerName);
+      Application := TFCGIApplication.Create(ServerName + ' ' + PrevVersion + ' - ' + GetEnvironment, TpitThread, StrToInt(GetIniParameter('Connection', 'Port', '2016')));
+      NoService   := not Exists;
+      Application.Icon := ImagePath + 'pitinnu16.ico';
+      Recover;
+      if Exists then
+        Run([TServiceThread.Create(true)])
+      else
+        Application.Run;
+      Snapshot;
+    end;
+  except
+    on E : Exception do ReportEventLog(EventError, 1, E.Message);
+  end;
+{$ELSE}
+  Prevalence  := TPrevalence.Create(ServerName);
+  Application := TFCGIApplication.Create(ServerName + ' ' + PrevVersion + ' - ' + GetEnvironment, TpitThread, StrToInt(GetIniParameter('Connection', 'Port', '2016')));
+  Application.Icon := ImagePath + 'pitinnu16.ico';
+  Recover;
+  Application.Run;
+  Snapshot;
+{$ENDIF}
+end.
