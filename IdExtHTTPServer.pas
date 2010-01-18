802d9c175c47e646931773e34d43eef159bb04cc
wanderlan.anjos <wanderlan.anjos@0bf252ff-4e48-0410-9925-dbe306d98b98>
2010-01-18 16:57:01 +0000

    Fix for garbage collector in Embedded WebServer

--- IdExtHTTPServer.pas
+++ IdExtHTTPServer.pas
@@ -416,7 +416,7 @@ var
   I: Integer;
 begin
   I := FGarbageCollector.IndexOfObject(Obj);
-  if I >= 0 then FGarbageCollector.Objects[I] := nil;
+  if I >= 0 then TExtObject(FGarbageCollector.Objects[I]).IsChild := true;
 end;
 
 procedure TIdExtSession.DeleteFromGarbage(Name: string);
